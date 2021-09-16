a5c97ee66216eb6c536f7b956552ae519d6d21d6
himanshu007-creator <addyjeridiq@gmail.com>
2021-09-16 18:21:35 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,23 @@
+var lang = document.getElementById("lang");
+var ull = document.getElementById("ull");
+lang.addEventListener("click", () => {
+  var ol = document.getElementById("langs");
+  ol.classList.toggle("no");
+  var bdy = document.querySelector("body");
+  //   if (!ol.classList.contains("no")) {
+  //     bdy.style.opacity = "0.5";
+  //     ull.style.opacity = "1";
+  //     ull.style.zIndex = "10";
+  //   } else {
+  //     bdy.style.opacity = "1";
+  //     ull.style.zIndex = "1";
+  //   }
+  var icon = document.getElementById("icon");
+  icon.classList.toggle("down");
+  icon.classList.toggle("up");
+});
+var smull = document.getElementById("small");
+smull.addEventListener("click", () => {
+  smull.classList.toggle("fa-user");
+  smull.classList.toggle("fa-cross");
+});
